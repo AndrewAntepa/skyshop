d6955524950833cbb221b262a2d76c46c81ceac9
AndrewAntepa <andrewantepa@gmail.com>
2025-01-24 01:27:32 +0800

    initial commit

---
+++ skyshop/src/main/java/org/skypro/skyshop/model/search/Searchable.java
@@ -0,0 +1,15 @@
+package org.skypro.skyshop.model.search;
+
+import java.util.UUID;
+
+public interface Searchable {
+    String getSearchTerm();
+
+    String getTypeContent();
+
+    default String getStringRepresentation(){
+        return getSearchTerm();
+    }
+
+    UUID getId();
+}
